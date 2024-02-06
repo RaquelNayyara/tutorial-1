c089cfce4e05525a627fb9a15184da40ca6a0bc6
RaquelNayyara <quelaazzz@gmail.com>
2024-02-06 20:09:39 +0700

    commit product

---
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Product.java
@@ -0,0 +1,12 @@
+package id.ac.ui.cs.advprog.eshop.model;
+
+import lombok.Setter;
+import lombok.Getter;
+
+@Getter @Setter
+public class Product {
+    private String productId;
+    private String productName;
+    private int productQuantity;
+}
+
