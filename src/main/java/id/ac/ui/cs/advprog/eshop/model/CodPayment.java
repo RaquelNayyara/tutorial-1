3c993e9fa0cb7da94cddbcb137e962eba20e056f
RaquelNayyara <125016793+RaquelNayyara@users.noreply.github.com>
2024-03-03 03:40:46 +0700

    [GREEN] Implement CodPayment model

--- src/main/java/id/ac/ui/cs/advprog/eshop/model/CodPayment.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/CodPayment.java
@@ -5,8 +5,27 @@ import java.util.Map;
 @Getter
 public class CodPayment extends Payment {
     public CodPayment(String id, String method, Order order, Map<String, String> paymentData, String status) {
+        super(id, method, order, paymentData, status);
     }
 
     public CodPayment(String id, String method, Order order, Map<String, String> paymentData) {
+        super(id, method, order, paymentData);
+    }
+
+    @Override
+    public void setPaymentData(Map<String, String> paymentData) {
+        if (paymentData == null || paymentData.isEmpty()) {
+            throw new IllegalArgumentException("Payment data cannot be empty");
+        }
+
+        if (paymentData.get("Address") == null || paymentData.get("Address").isEmpty()) {
+            throw new IllegalArgumentException("Address cannot be empty");
+        }
+
+        if (paymentData.get("deliveryFee") == null || paymentData.get("deliveryFee").isEmpty()) {
+            throw new IllegalArgumentException("Delivery fee cannot be empty");
+        }
+
+        this.paymentData = paymentData;
     }
 }
