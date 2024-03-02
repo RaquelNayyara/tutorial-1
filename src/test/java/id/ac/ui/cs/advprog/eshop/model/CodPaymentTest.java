3326ec3320c10b4436def68a09b34442a4af5b6c
RaquelNayyara <125016793+RaquelNayyara@users.noreply.github.com>
2024-03-03 03:53:05 +0700

    [REFACTOR] Fix import

--- src/test/java/id/ac/ui/cs/advprog/eshop/model/CodPaymentTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/model/CodPaymentTest.java
@@ -1,7 +1,7 @@
 package id.ac.ui.cs.advprog.eshop.model;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertSame;
-
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
