278c60808130c1934344e8d2163817b51157a912
RaquelNayyara <125016793+RaquelNayyara@users.noreply.github.com>
2024-03-05 23:46:35 +0700

    [GREEN] implemented PaymentRepository

---
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/PaymentRepository.java
@@ -0,0 +1,38 @@
+package id.ac.ui.cs.advprog.eshop.repository;
+import id.ac.ui.cs.advprog.eshop.model.Payment;
+import org.springframework.stereotype.Repository;
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
+public class PaymentRepository {
+    private List<Payment> payments = new ArrayList<>();
+
+    public Payment save(Payment payment) {
+        int i = 0;
+        for (Payment p : payments) {
+            if (p.getId().equals(payment.getId())) {
+                payments.set(i, payment);
+                return payment;
+            }
+            i++;
+        }
+
+        payments.add(payment);
+        return payment;
+    }
+
+    public Payment findById(String id) {
+        for (Payment p : payments) {
+            if (p.getId().equals(id)) {
+                return p;
+            }
+        }
+
+        return null;
+    }
+
+    public List<Payment> getAllPayments() {
+        return payments;
+    }
+}
